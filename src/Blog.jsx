09c46fc9dae2d91d68ed5aca699fa9039ac78c69
Choon Gee <cgee.code@gmail.com>
2024-08-21 20:42:52 +0800

    ci: initial setup of ci workflows

---
+++ src/Blog.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+
+import { PostList } from './components/PostList'
+import { CreatePost } from './components/CreatePost'
+import { PostFilter } from './components/PostFilter'
+import { PostSorting } from './components/PostSorting'
+import { getPosts } from './api/posts.js'
+
+export function Blog() {
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOrder] = useState('descending')
+
+  const postQuery = useQuery({
+    queryKey: ['posts', { author, sortBy, sortOrder }],
+    queryFn: () => getPosts({ author, sortBy, sortOrder }),
+  })
+  const posts = postQuery.data ?? []
+
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      FilterBy:
+      <PostFilter field='author' onChange={(value) => setAuthor(value)} />
+      <br />
+      <PostSorting
+        fields={['createdAt', 'updatedAt']}
+        value={sortBy}
+        onChange={(value) => setSortBy(value)}
+        orderValue={sortOrder}
+        onOrderChange={(orderValue) => setSortOrder(orderValue)}
+      />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
+}
